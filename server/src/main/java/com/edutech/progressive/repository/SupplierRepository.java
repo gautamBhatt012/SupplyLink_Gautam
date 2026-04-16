231ba37aa0f86d67a04908de13c9aae826c175df
gautamBhatt012 <gautam2002bhatt@gmail.com>
2026-04-16 15:30:20 +0000

    day_12

--- server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
@@ -1,8 +1,35 @@
+// package com.edutech.progressive.repository;
+
+// import java.util.Optional;
+
+// import javax.transaction.Transactional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Modifying;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+// import org.springframework.stereotype.Repository;
+
+// import com.edutech.progressive.entity.Supplier;
+
+// @Repository
+// public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
+//     @Modifying
+//     @Transactional
+//     @Query("delete from Supplier s where s.supplierId = :supplierId")
+//     void deleteBySupplierId(@Param("supplierId") int supplierId);
+    
+//     @Query("select s from Supplier s where s.supplierId = :supplierId")
+//     Supplier findBySupplierId(@Param("supplierId") int supplierId);
+    
+//     @Query("select s from Supplier s where s.supplierName = :supplierName")
+//     Supplier findByUsername(@Param("supplierName") String username);
+     
+//     @Query("select s from Supplier s where s.email = :email")
+//     Supplier findByEmail(@Param("email") String email);
+// }
+
 package com.edutech.progressive.repository;
-
-import java.util.Optional;
-
-import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,18 +40,15 @@ import org.springframework.stereotype.Repository;
 import com.edutech.progressive.entity.Supplier;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
     @Modifying
-    @Transactional
-    @Query("delete from Supplier s where s.supplierId = :supplierId")
+    @Query("DELETE FROM Supplier s WHERE s.supplierId = :supplierId")
     void deleteBySupplierId(@Param("supplierId") int supplierId);
-    
-    @Query("select s from Supplier s where s.supplierId = :supplierId")
-    Supplier findBySupplierId(@Param("supplierId") int supplierId);
-    
-    @Query("select s from Supplier s where s.supplierName = :supplierName")
-    Supplier findByUsername(@Param("supplierName") String username);
-     
-    @Query("select s from Supplier s where s.email = :email")
-    Supplier findByEmail(@Param("email") String email);
+
+    Supplier findBySupplierId(int supplierId);
+
+    Supplier findByUsername(String username);
+
+    Supplier findByEmail(String email);
 }
